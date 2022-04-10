ea7dfd822cf8959ea48646b39ca30a56ef48a9c6
Hafidzrdwn <hafidz10032005@gmail.com>
2022-04-10 16:07:03 +0700

    change config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/reaction-timer-app/' : '/',
   transpileDependencies: true,
-  devServer: {
-    host: 'localhost',
-    port: 3000
-  }
 })
