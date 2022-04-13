72ec6173ccea0eef1cb0db079bbd03636aeb5e9a
Hafidzrdwn <hafidz10032005@gmail.com>
2022-04-14 02:39:32 +0700

    add reset game button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <h1><fa :icon="['fab', 'vuejs']" /> Reaction Timer App</h1>
-  <button @click="start" :disabled="isPlaying">
+  <button id="btnStart" @click="start" :disabled="isPlaying">
     {{ btnText }}&nbsp;<fa :icon="btnTextIcon"/>
+  </button>
+  <button id="btnReset" v-if="showResult" @click="reset">
+    {{ btnResetText }}
   </button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
   <Result v-if="showResult" :score="score" />
 </template>
 
 <script>
-import Block from "./components/Block.vue";
-import Result from "./components/Result.vue";
+import Block from "./components/Block.vue"
+import Result from "./components/Result.vue"
 
 export default {
   name: "App",
@@ -25,23 +28,29 @@ export default {
       score: null,
       btnText: "Let's play",
       btnTextIcon: "circle-play",
+      btnResetText: "Reset game"
     };
   },
   methods: {
     start() {
-      this.delay = Math.round(2000 + Math.random() * 2000);
-      this.showResult = false;
-      this.isPlaying = true;
-      this.btnText = "Let's play";
-      this.btnTextIcon = "circle-play";
+      this.delay = Math.round(2000 + Math.random() * 2000)
+      this.showResult = false
+      this.isPlaying = true
+      this.btnText = "Let's play"
+      this.btnTextIcon = "circle-play"
     },
     endGame(reactionTime) {
-      this.showResult = true;
-      this.score = reactionTime;
-      this.isPlaying = false;
-      this.btnText = "Again";
-      this.btnTextIcon = "arrow-rotate-left";
+      this.showResult = true
+      this.score = reactionTime
+      this.isPlaying = false
+      this.btnText = "Again"
+      this.btnTextIcon = "arrow-rotate-left"
     },
+    reset() {
+      this.showResult = false
+      this.btnText = "Let's play"
+      this.btnTextIcon = "circle-play"
+    }
   },
 };
 </script>
@@ -66,20 +75,38 @@ button {
   padding: 12px 30px;
   border: 0;
   border-radius: 6px;
-  background: #55c794;
-  color: white;
   cursor: pointer;
   font-size: 15px;
-  box-shadow: 0 3px 15px #55c794d2;
-  margin: 10px 0;
+  margin: 10px 12px;
+  transition: .3s;
 }
 
-button:hover {
+#btnStart {
+  background: #55c794;
+  color: white;
+  box-shadow: 0 3px 15px #55c794d2;
+}
+
+#btnStart:hover {
   background: #42b883;
 }
 
-button:active {
+#btnStart:active {
   box-shadow: 0 3px 12px #55c794;
+}
+
+#btnReset {
+  background: #dc3545;
+  color: white;
+  box-shadow: 0 3px 15px #dc3546a2;
+}
+
+#btnReset:hover {
+  background: #cd2435;
+}
+
+#btnReset:active {
+  box-shadow: 0 3px 12px #dc3545;
 }
 
 button:disabled {
